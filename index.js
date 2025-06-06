df64591c46bcd41c9bf1e23f4ca44c8803967744
Iamkorir3 <korir5956@gmail.com>
2025-06-06 23:29:51 +0300

    required js for react

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ root.render(
     <App />
   </StrictMode>
 );
+
+const { title, author, pages, publicationDate, genres } = book;
+title;
+author;
+console.log(title, author, pages, publicationDate, genres);
+
+// genres
+// const primaryGenre = genres[0];
+// const secondGenre = genres[1];
+const [primaryGenre, secondGenre, ...otherGenres] = genres;
+console.log(primaryGenre, secondGenre, otherGenres);
+
+const newGenres = [...genres, "epic", "fantsy"];
+newGenres;
+
+const updatedBook = {
+  ...book,
+  //Adding a property
+  moviePublication: "2001-12-19",
+
+  //Overriding a property
+  pages: "1200",
+};
+updatedBook;
+
+const summary = `${title} is a  book`;
+summary;
+
+const pagesRnge =
+  pages > 1000 ? "pages over a thousand" : "less than a thousand";
+pagesRnge;
